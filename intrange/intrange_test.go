b4d2368805b8eea5b9d7e97b69998ab05e257357
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-05-09 16:06:23 -0500

    Unit tests for int range

--- intrange/intrange_test.go
+++ intrange/intrange_test.go
@@ -1,5 +1,12 @@
 package intrange
 
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
 // There was a previous implementation of spread
 // which was not a different way of initializing linear
 // and this test confirmed that they were equivalent.
@@ -35,3 +42,38 @@ package intrange
 // 	fmt.Println(lTot / 1000)
 // 	fmt.Println(bsTot / 1000)
 // }
+
+func TestInfinite(t *testing.T) {
+	i := NewInfinite()
+	assert.Equal(t, i.Poll(), math.MaxInt32)
+	i = i.Mult(5)
+	assert.Equal(t, i.Poll(), math.MaxInt32)
+}
+
+func TestConstant(t *testing.T) {
+	c := NewConstant(5)
+	assert.Equal(t, c.Poll(), 5)
+	c = c.Mult(5)
+	assert.Equal(t, c.Poll(), 25)
+}
+
+func TestLinear(t *testing.T) {
+	l := NewLinear(0, 10)
+	assert.True(t, l.Poll() < 11)
+	assert.True(t, l.Poll() > -1)
+	l = l.Mult(2)
+	assert.True(t, l.Poll() < 21)
+	l = NewLinear(10, 0)
+	assert.True(t, l.Poll() < 11)
+	assert.True(t, l.Poll() > -1)
+	l = NewLinear(0, 0)
+	assert.Equal(t, l.Poll(), 0)
+	l = NewSpread(5, 5)
+	assert.True(t, l.Poll() < 11)
+	assert.True(t, l.Poll() > -1)
+	l = NewSpread(5, -5)
+	assert.True(t, l.Poll() < 11)
+	assert.True(t, l.Poll() > -1)
+	l = NewSpread(5, 0)
+	assert.Equal(t, l.Poll(), 5)
+}
