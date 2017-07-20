f83c05b9ce8259e8416db408981657338757f01a
StephenPatrick <sythe2o0@gmail.com>
2017-07-20 11:03:01 -0500

    Split linear color into Linear64 and Linear

--- colorrange/linear.go
+++ colorrange/linear.go
@@ -7,6 +7,52 @@ import (
 )
 
 // Linear color ranges return colors on a linear distribution
+type Linear64 struct {
+	r, g, b, a intrange.Range
+}
+
+// NewLinear64 returns a linear color distribution between min and maxColor
+func NewLinear64(minColor, maxColor color.Color) Range {
+	r, g, b, a := minColor.RGBA()
+	r2, g2, b2, a2 := maxColor.RGBA()
+	return Linear64{
+		intrange.NewLinear(int(r), int(r2)),
+		intrange.NewLinear(int(g), int(g2)),
+		intrange.NewLinear(int(b), int(b2)),
+		intrange.NewLinear(int(a), int(a2)),
+	}
+}
+
+// EnforceRange rounds the input color's components so that they fall in the
+// given range.
+func (l Linear64) EnforceRange(c color.Color) color.Color {
+	r3, g3, b3, a3 := c.RGBA()
+	r4 := l.r.EnforceRange(int(r3))
+	g4 := l.g.EnforceRange(int(g3))
+	b4 := l.b.EnforceRange(int(b3))
+	a4 := l.a.EnforceRange(int(a3))
+	return color.RGBA64{uint16(r4), uint16(g4), uint16(b4), uint16(a4)}
+}
+
+// InRange returns whether the input color falls in this range
+func (l Linear64) InRange(c color.Color) bool {
+	r3, g3, b3, a3 := c.RGBA()
+	return l.r.InRange(int(r3)) &&
+		l.g.InRange(int(g3)) &&
+		l.b.InRange(int(b3)) &&
+		l.a.InRange(int(a3))
+}
+
+// Poll returns a randomly chosen color in the bounds of this color range
+func (l Linear64) Poll() color.Color {
+	r3 := l.r.Poll()
+	g3 := l.g.Poll()
+	b3 := l.b.Poll()
+	a3 := l.a.Poll()
+	return color.RGBA64{uint16(r3), uint16(g3), uint16(b3), uint16(a3)}
+}
+
+// Linear color ranges return colors on a linear distribution
 type Linear struct {
 	r, g, b, a intrange.Range
 }
@@ -31,7 +77,7 @@ func (l Linear) EnforceRange(c color.Color) color.Color {
 	g4 := l.g.EnforceRange(int(g3))
 	b4 := l.b.EnforceRange(int(b3))
 	a4 := l.a.EnforceRange(int(a3))
-	return color.RGBA64{uint16(r4), uint16(g4), uint16(b4), uint16(a4)}
+	return color.RGBA{uint8(r4 / 255), uint8(g4 / 255), uint8(b4 / 255), uint8(a4 / 255)}
 }
 
 // InRange returns whether the input color falls in this range
@@ -49,5 +95,5 @@ func (l Linear) Poll() color.Color {
 	g3 := l.g.Poll()
 	b3 := l.b.Poll()
 	a3 := l.a.Poll()
-	return color.RGBA64{uint16(r3), uint16(g3), uint16(b3), uint16(a3)}
+	return color.RGBA{uint8(r3 / 255), uint8(g3 / 255), uint8(b3 / 255), uint8(a3 / 255)}
 }
