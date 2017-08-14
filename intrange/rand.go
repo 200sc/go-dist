4d378fe8135f1f8fe5959530fa7a5779fb7a76aa
StephenPatrick <sythe2o0@gmail.com>
2017-08-14 15:42:02 -0500

    Change to rand generation to sleep, to make sure no one gets the same seeded rand, but it's insufficient

--- intrange/rand.go
+++ intrange/rand.go
@@ -6,5 +6,8 @@ import (
 )
 
 func nowRand() *rand.Rand {
+	// We need to make sure we don't get the same rand source for
+	// two ranges
+	time.Sleep(1 * time.Millisecond)
 	return rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
 }
